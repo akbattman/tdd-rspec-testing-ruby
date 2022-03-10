c3f17c01142077ac1c490df9bc1b26cbe46b9013
Aaron Battman <aaron.k.battman@outlook.com>
2022-03-10 10:12:02 +0000

    rubocop syntaxes ammended & literal string magic comment -Autoed

--- rspec-101/spec/acronymize_spec.rb
+++ rspec-101/spec/acronymize_spec.rb
@@ -1,9 +1,11 @@
-require_relative "../acronymize" # library / file to test
+# frozen_string_literal: true
 
-describe "#acronymize" do
+require_relative '../acronymize' # library / file to test
+
+describe '#acronymize' do
   it "returns FAQ for 'Frequently Asked Questions'" do
-    actual = acronymize("Frequently Asked Questions") # call the code
-    expected = "FAQ" # expected result
+    actual = acronymize('Frequently Asked Questions') # call the code
+    expected = 'FAQ' # expected result
 
     # RSpec to compare both:
     expect(actual).to eq(expected)
