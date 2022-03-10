057c3a62eb2278d011012be01e98c35b54ad6760
Aaron Battman <aaron.k.battman@outlook.com>
2022-03-10 16:13:38 +0000

    small refactor to enumerable

--- rspec-101-func-test/acronymize.rb
+++ rspec-101-func-test/acronymize.rb
@@ -2,5 +2,6 @@
 
 # function to write / tdd
 def acronymize(sentence)
-  sentence.split(" ").map { |word| word[0] }.join.upcase
+  # sentence.split(" ").map { |word| word[0] }.join.upcase
+  sentence.split(" ").map(&:chr).join.upcase
 end
