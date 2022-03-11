a4c84b31e2da6ee212777e2a6151a93912b86c99
Aaron Battman <aaron.k.battman@outlook.com>
2022-03-11 15:45:34 +0000

    low score game test & class build with simple sum instance meth

--- its-just-not-cricket/bowling.rb
+++ its-just-not-cricket/bowling.rb
@@ -1,1 +1,13 @@
 # frozen_string_literal: true
+
+class Bowling
+  attr_reader :score
+
+  def initialize
+    @score = 0
+  end
+
+  def roll(pins_hit)
+    @score += pins_hit
+  end
+end
--- its-just-not-cricket/spec/bowling_spec.rb
+++ its-just-not-cricket/spec/bowling_spec.rb
@@ -1,1 +1,18 @@
 # frozen_string_literal: true
+
+require_relative '../bowling' #file to test
+
+# describe Bowling do
+#   describe '#score' do
+describe Bowling, '#score' do
+  context 'with no strikes or spares' do
+    it 'sums the pin count for each roll' do
+      bowling = Bowling.new
+      
+      20.times { bowling.roll(4) } # hard code a pin count to get the ball rolling 
+
+      expect(bowling.score).to eq 80
+    end
+  end
+end
+# end
