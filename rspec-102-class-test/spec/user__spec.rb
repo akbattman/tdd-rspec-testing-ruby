e6f3b73636acc3015dd2ca5e9c0c97de5792e3b9
Aaron Battman <aaron.k.battman@outlook.com>
2022-03-11 09:23:05 +0000

    class test written for concat & capi. fro-lits mag comm. cop quotations

---
+++ rspec-102-class-test/spec/user__spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative '../user' # file to test
+
+describe User do # additinal describe layer to target class
+  describe '#full_name' do # class method target
+    it "returns 'Aaron Battman'" do # method return I will code for
+      user = User.new('aaron', 'battman')
+
+      actual = user.full_name # call the class method
+      expected = 'Aaron Battman' # expected result as per 'it'
+
+      expect(actual).to eq(expected) # RSpec comparison
+    end
+  end
+end
---
+++ rspec-102-class-test/user.rb
@@ -0,0 +1,3 @@
+# frozen_string_literal: true
+
+# function to write / tdd
